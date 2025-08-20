fee4ebfd7083bc563a91156bf62ad27ad81f5be0
Jnglct <rogerscole99@gmail.com>
2025-08-20 15:41:03 -0500

    Add project files.

---
+++ Assignment7.4.1/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Assignment7._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingSystem airportParking = new ParkingSystem();
+            airportParking.parkingSystem(2, 0, 1);
+            airportParking.addCar(1);
+            airportParking.addCar(2);
+            airportParking.addCar(3);
+            airportParking.addCar(1); 
+            airportParking.addCar(1);
+        }
+    }
+    public class ParkingSystem
+    {
+        public int BigCar;
+        public int MediumCar;
+        public int SmallCar;
+
+        public ParkingSystem()
+        {
+            BigCar = 0;
+            MediumCar = 0;
+            SmallCar = 0;
+        }
+
+        public void parkingSystem(int big, int medium, int small)
+        {
+            BigCar = big;
+            MediumCar = medium;
+            SmallCar = small;
+        }
+        public bool addCar(int car)
+        {
+            switch(car)
+            {
+                case 1:
+                    if (BigCar > 0)
+                    {
+                        BigCar--;
+                        Console.WriteLine("Big car parked.");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No space for big car.");
+                    }
+                    break;
+                case 2:
+                    if (MediumCar > 0)
+                    {
+                        MediumCar--;
+                        Console.WriteLine("Medium car parked.");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No space for medium car.");
+                    }
+                    break;
+                case 3:
+                    if (SmallCar > 0)
+                    {
+                        SmallCar--;
+                        Console.WriteLine("Small car parked.");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("No space for small car.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid car type.");
+                    break;
+            }
+            return false;
+        }
+    }
+}
